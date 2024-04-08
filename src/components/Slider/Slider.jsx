0ae93d69e01cdc193b081abddaf4aeb7a7ce06e7
Joy <github.joysaha@gmail.com>
2024-04-08 23:48:38 +0600

    swipper slider edit and bug fixing

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,16 +1,16 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState } from "react";
 // Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
-import './slider.css';
+import "./slider.css";
 
 // import required modules
-import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
 
 export default function App() {
   return (
@@ -30,10 +30,18 @@ export default function App() {
         modules={[Autoplay, Pagination, Navigation]}
         className="mySwiper"
       >
-        <SwiperSlide><div className='slide slide1'>Slide 1</div></SwiperSlide>
-        <SwiperSlide><div className='slide slide2'>Slide 2</div></SwiperSlide>
-        <SwiperSlide><div className='slide slide3'>Slide 3</div></SwiperSlide>
-        <SwiperSlide><div className='slide slide4'>Slide 4</div></SwiperSlide>
+        <SwiperSlide>
+          <div className="slide slide1">Slide 1</div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="slide slide2">Slide 2</div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="slide slide3">Slide 3</div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="slide slide4">Slide 4</div>
+        </SwiperSlide>
       </Swiper>
     </>
   );
