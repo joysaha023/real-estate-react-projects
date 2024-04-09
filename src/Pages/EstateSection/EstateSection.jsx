f0cb4816404a0d804919fbd821b3d527bdb97b1b
Joy <github.joysaha@gmail.com>
2024-04-09 10:06:29 +0600

    create json data set json data

---
+++ src/Pages/EstateSection/EstateSection.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const EstateSection = () => {
+  return (
+    <div className="max-w-6xl mx-auto">
+      <div className="mt-8 md:mt-16 ">
+        <h1 className="text-center font-bold text-3xl uppercase">
+        Residential <span className="text-[#0077be]">Estate</span>
+        </h1>
+        <p className="w-3/4 mx-auto text-sm md:text-base font-normal mt-3 md:mt-5 text-center">
+        Welcome to the Residential Estate section at HavenHQ, where your dream of home finds its perfect match. Nestled amidst serene landscapes and thoughtfully designed neighborhoods, our residential estates offer a harmonious blend of luxury, comfort, and community.
+        </p>
+      </div>
+      <div>
+        
+      </div>
+    </div>
+  );
+};
+
+export default EstateSection;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Slider from '../../components/Slider/Slider';
 import AboutProperty from '../../components/AboutProperty/AboutProperty';
+import EstateSection from '../EstateSection/EstateSection';
 
 const Home = () => {
     return (
         <div>
             <Slider></Slider>
             <AboutProperty></AboutProperty>
+            <EstateSection></EstateSection>
         </div>
     );
 };
