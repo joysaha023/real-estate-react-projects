bbcb29e2b810ea7dfc0203658aad5da9cbbb3282
Joy <github.joysaha@gmail.com>
2024-04-11 23:41:58 +0600

    agents page design edit

--- src/Pages/Agents/Agents.jsx
+++ src/Pages/Agents/Agents.jsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import { Helmet } from 'react-helmet-async';
+import React from "react";
+import { Helmet } from "react-helmet-async";
 import { useLoaderData } from "react-router-dom";
-import AgentsCard from './AgentsCard';
+import AgentsCard from "./AgentsCard";
 
 const Agents = () => {
-    const agents = useLoaderData();
-    // console.log(agents)
-    return (
-        <div>
-            <Helmet>
-                <title>HavenHQ - Agents</title>
-            </Helmet>
-            <h2>This is agents</h2>
-            <div>
-                {
-                    agents.map(item => <AgentsCard key={item} item={item}></AgentsCard>)
-                }
-            </div>
-        </div>
-    );
+  const agents = useLoaderData();
+  // console.log(agents)
+  return (
+    <div className="max-w-6xl mx-auto">
+      <Helmet>
+        <title>HavenHQ - Agents</title>
+      </Helmet>
+      <div className="mt-2 md:mt-4 ">
+        <h1 className="text-center font-bold text-3xl uppercase">
+          Meet Our <span className="text-[#0077be]">Agents</span>
+        </h1>
+        <p className="w-3/4 mx-auto text-sm md:text-base font-normal mt-3 md:mt-5 text-center">
+          Meet and contact our popular agents and talk to then about real estate
+          and know about how to book and buy or rent. please also try only email
+          and phone number and talk to them.
+        </p>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-4">
+        {agents.map((item) => (
+          <AgentsCard key={item} item={item}></AgentsCard>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Agents;
--- src/Pages/Agents/AgentsCard.jsx
+++ src/Pages/Agents/AgentsCard.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React from "react";
 
-const AgentsCard = ({item}) => {
-    console.log(item)
-    return (
-        <div>
-            <h2>hello</h2>
-        </div>
-    );
+const AgentsCard = ({ item }) => {
+  const { name, email, phone, address, license, image } = item;
+  return (
+    <div className="card md:card-side bg-base-100 border rounded-none">
+      <figure className="p-4">
+        <img
+          src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
+          alt="Movie"
+        />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title text-2xl">{name}</h2>
+        <span>Phone: {phone}</span>
+        <span>License: {license}</span>
+        <span>Address: {address}</span>
+        <span>Email: {email}</span>
+        
+      </div>
+    </div>
+  );
 };
 
 export default AgentsCard;
